dc95616ef1c6dbe214d08717b4b0747223d40f1f
Suraj Pathak <freesuraj@gmail.com>
2016-02-03 14:31:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### SPTinderView
+
+A clone of standard Tinder app's swipe left swipe right view [WIP]
+
+#### TODO
+- Add multiple gestures
+- Add auto-increasing/decreasing cells album
+- Add animating buttons
+- Write Unit test
+
+#### Contribution
+
+All the contributions are welcome ! Fork, change and send a pull request.
+
---
+++ SPTinderView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPTinderView
+//
+//  Created by Suraj Pathak on 3/2/16.
+//  Copyright © 2016 Suraj Pathak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
